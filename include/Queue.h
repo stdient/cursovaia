deb01e4c5cac1730f155f6c8aa52509c565911b5
hikaruvi <daniilrybkin32@gmail.com>
2025-02-01 17:29:10 +0700

    feat: add find vertex in tree

--- include/Queue.h
+++ include/Queue.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <iostream>
+#include <math.h>
 
 typedef unsigned char byte;
 
-typedef struct {
+typedef struct
+{
     char citizen_full_name[32];
     char street_name[18];
     short int house_number;
@@ -10,15 +13,46 @@ typedef struct {
     char date_of_move_in[10];
 } Tdata;
 
-typedef struct {
+typedef struct Record
+{
     char citizen_full_name[32 * 2];
     char street_name[18 * 2];
     short int house_number;
     short int apartment_number;
     char date_of_move_in[10];
+
+    friend std::ostream &operator<<(std::ostream &os, const Record &record)
+    {
+        os << record.citizen_full_name << '|';
+        os << record.street_name << '|';
+        os << record.house_number << '|';
+        os << record.apartment_number;
+
+        for (int i = 0; i < 2; ++i)
+        {
+            if (record.apartment_number < pow(10, i + 1))
+                os << ' ';
+        }
+
+        os << '|';
+        os << record.date_of_move_in;
+
+        return os;
+    }
+
+    bool operator<(const Record &other)
+    {
+        return this->house_number < other.house_number;
+    }
+
+    bool operator>(const Record &other)
+    {
+        return this->house_number > other.house_number;
+    }
 } Record;
 
-typedef struct Node {
+typedef struct Node
+{
     union {
         Record data;
         byte digit[sizeof(Record)];
@@ -26,7 +60,8 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-typedef struct {
+typedef struct
+{
     Node *head;
     Node *tail;
 } Queue;
--- include/String.h
+++ include/String.h
@@ -5,6 +5,5 @@ char *convertStr(char *str);
 size_t mstrlen(const char *str);
 char *mstrcat(char *dest, char *src);
 void mstrcpy(char *toHere, char *fromHere);
-// TODO несколько разграничителей подряд
 char *mstrtok(char *s, const char *delim);
 int convertStrToInt(char *str);
